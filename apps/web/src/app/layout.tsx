aac311417d8199d8e005f27b933ed8913a173e99
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2026-01-01 17:58:56 +0100

    Add @next/third-parties dependency and integrate Google Analytics into layout
    - Added @next/third-parties package with version 16.1.1 to package.json.
    - Integrated Google Analytics component into the RootLayout for tracking.
    - Included Cookiebot script for consent management in the layout.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,35 +6,48 @@ import { ThemeProvider } from "../components/theme-provider";
 import { UserProvider } from "../components/user-provider";
 import { ThemeScript } from "./theme-script";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const baloo2 = Baloo_2({
-  variable: "--font-baloo-2",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800"],
+    variable: "--font-baloo-2",
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
-  title: "Entarat - The Modern Way to Play",
-  description:
-    "Entarat is the modern way to play. It's a platform for playing games with your friends and family.",
+    title: "Entarat - The Modern Way to Play",
+    description:
+        "Entarat is the modern way to play. It's a platform for playing games with your friends and family.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${baloo2.variable} antialiased`}>
-        <ThemeScript />
-        <ThemeProvider defaultTheme="dark" storageKey="entarat-theme">
-          <UserProvider>
-            <GameSetupProvider>{children}</GameSetupProvider>
-          </UserProvider>
-        </ThemeProvider>
-        <ConditionalFooter />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={`${baloo2.variable} antialiased`}>
+                <ThemeScript />
+                <ThemeProvider defaultTheme="dark" storageKey="entarat-theme">
+                    <UserProvider>
+                        <GameSetupProvider>{children}</GameSetupProvider>
+                    </UserProvider>
+                </ThemeProvider>
+                <ConditionalFooter />
+            </body>
+            <GoogleAnalytics gaId="G-VQLETD20GH" />
+            {
+              process.env.NODE_ENV === "production" && (
+                <Script
+                id="Cookiebot"
+                src="https://consent.cookiebot.com/uc.js"
+                data-cbid="b6a238b3-62cc-4274-bd57-833ed7c74791"
+                type="text/javascript"
+                async
+                />
+              )}
+        </html>
+    );
 }
